e4442291221b633d7ef50186e474063a42532f77
costa-rica <nrodrig1@gmail.com>
2023-08-14 20:44:52 +0200

    Initial Commit

---
+++ UITableViewCell05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewCell05
+//
+//  Created by Nick Rodriguez on 14/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
